afdf8a08c5d2d93696f79f2c5de8bb033776380c
Sharmina Akhtar <sakht318@gmail.com>
2018-04-15 23:43:48 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
      @student = Student.find(params[:id])
      @student.active = !@student.active
      @student.save
-     redirect student_path(@student)
+     redirect_to student_path(@student)
    end
 
   private
